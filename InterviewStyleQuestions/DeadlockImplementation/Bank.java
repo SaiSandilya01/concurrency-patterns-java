43ed7704f3402846f3437b52ce62d04a01860a6c
Sai Sandilya V <sandilya.2001@gmail.com>
2026-05-10 11:43:02 +0530

    Simulated deadlock and resolved it

---
+++ InterviewStyleQuestions/DeadlockImplementation/Bank.java
@@ -0,0 +1,19 @@
+package InterviewStyleQuestions.DeadlockImplementation;
+
+public class Bank {
+
+    int balance;
+
+    Bank(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+}
---
+++ InterviewStyleQuestions/DeadlockImplementation/Deadlock.java
@@ -0,0 +1,76 @@
+package InterviewStyleQuestions.DeadlockImplementation;
+
+public class Deadlock {
+
+    public static void bankTransferAB(Bank A, Bank B) throws InterruptedException {
+
+        synchronized (A) {
+            Thread.sleep(2000);
+            synchronized (B) {
+                int temp = A.getBalance();
+                B.setBalance(B.getBalance() + temp);
+                A.setBalance(0);
+
+            }
+        }
+
+    }
+
+    public static void bankTransferBA(Bank B, Bank A) throws InterruptedException {
+
+        synchronized (B) {
+            Thread.sleep(2000);
+            synchronized (A) {
+                int temp = B.getBalance();
+                A.setBalance(A.getBalance() + temp);
+                B.setBalance(0);
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Bank A = new Bank(100);
+        Bank B = new Bank(150);
+
+        Thread a = new Thread(() -> {
+            try {
+                Deadlock.bankTransferAB(A, B);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
+
+        Thread b = new Thread(() -> {
+            try {
+                Deadlock.bankTransferBA(B, A);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
+
+        a.start();
+        b.start();
+
+        try {
+            a.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            b.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("Execution done");
+
+    }
+
+}
